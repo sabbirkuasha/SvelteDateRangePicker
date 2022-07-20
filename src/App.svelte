b748cc1b69f8e340d87834b535960958f693a6b9
Sabbirz <sabbir.kuasha@gmail.com>
2022-07-20 15:11:26 +0600

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import DatePicker from "./DatePicker.svelte";
+  let DateSelected = ''
+
+
+</script>
+
+<main class="flex flex-col space-y-5 justify-center items-center h-screen bg-black">
+  
+  <!-- child to parent - Sab -->
+  <DatePicker onChange={newValue => 
+      {
+        DateSelected = newValue; 
+        console.log(DateSelected)
+      }}/>
+
+
+  <input type="text" id="dateRange" value="" class="w-1/2 bg-orange-700 text-white font-bold" />
+  
+  <!-- child to parent - Sab -->
+  <div>
+      Selected Date is <b>{DateSelected}</b> 
+  </div>
+
+</main>
+
+<style>
+
+</style>
---
+++ src/DatePicker.svelte
@@ -0,0 +1,48 @@
+<script>
+import jQuery from 'jquery'
+import moment from 'moment'
+import daterangepicker from 'daterangepicker'
+   
+
+    export let dateContainerID = "#dateRange"
+
+
+    //child to parent - Sab
+    export let onChange
+    let DateSelected = ''
+    $: onChange(DateSelected)
+
+  
+    jQuery(function()
+    {
+      jQuery((dateContainerID)).daterangepicker
+        (
+          {
+            ranges: 
+            {
+                'opens': 'center',
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            },
+            
+            "alwaysShowCalendars": true,
+            "startDate": moment(),
+            "endDate": moment(),
+            "opens": "right",
+          }, function(start, end) 
+            {
+                //child to parent - Sab
+                DateSelected = start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD')
+                
+                console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'))
+            
+            }
+        )
+    })
+
+    
+  </script>
